81fb2b7fa20c0618356a3dd019339ef6a43f5eb4
Kaio <kakacnmendes@gmail.com>
2025-03-06 16:37:40 -0300

    Verifica os conectivos da formula

--- analisadorSintatico.js
+++ analisadorSintatico.js
@@ -1,8 +1,4 @@
-function analisadorSintatico(formula) {
-
-}
-
-let formula = "(A^B"
+let formula = "A^↔B"
 
 function verificaParenteses(formula) {
     let parentesesFecha = [] // controla os parenteses.
@@ -29,11 +25,43 @@ function verificaParenteses(formula) {
             // se nao for o primeiro valor, significa que os parenteses estão ok.
             parentesesFecha.pop()   
         }
+
         
     }
 
     if (parentesesFecha.length > 0) return { valido: false, erro: "Parênteses desbalanceados" };
     return { valido: true };
+
 }
 
-console.log(verificaParenteses(formula))
+function analisadorSintatico(formula) {
+
+    // verifica se os parenteses estão balanceados
+    const isParentesesOk = verificaParenteses(formula)
+
+    // se não estiverem, já retorna a fórmula como inválida.
+    if (!isParentesesOk.valido) {
+        return { valido: false, erro: isParentesesOk.erro }
+    }
+
+    // armazena o caractere anterior
+    let anterior = '';
+
+    // percorre a fórmula.
+    for (let i = 0; i < formula.length; i++) {
+        let char =  formula[i]
+        // console.log(`[${i}]: ${'^∨→↔∨/~'.includes(char) && '^∨→↔∨/~'.includes(anterior)}`)
+
+        // verifica se o caractere atual é um conectivo igual ao caractere anterior
+        if ('^∨→↔∨/~'.includes(char) && '^∨→↔∨/~'.includes(anterior)) {
+            return { valido: false, erro:"Conectivos em sequência." }
+        }
+
+        // atualiza o valor do caractere anterior para a próxima interação
+        anterior = char;
+    }
+
+    return { valido: true };
+}
+
+console.log(analisadorSintatico(formula))
